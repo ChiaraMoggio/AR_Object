38e05c44fc461b114a8f6833005e11b1d988bb0e
Chiara Moggio <chiaramoggio1997@gmail.com>
2025-07-22 17:12:44 +0200

    Initial commit

---
+++ Assets/Script/TapToPlace.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TapToPlace : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    private ARRaycastManager raycastManager;
+    private GameObject spawnedObject;
+
+    void Start()
+    {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+
+                if (spawnedObject == null)
+                {
+                    spawnedObject = Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+                }
+                else
+                {
+                    spawnedObject.transform.position = hitPose.position;
+                }
+            }
+        }
+    }
+}
